3c1197df08191e4182870870ac882178f8d87b3d
Dirk <incentivetoken@gmail.com>
2017-04-22 16:53:35 +0200

    Horribly messy test

--- typescript-generator-core/src/test/java/cz/habarta/typescript/generator/NashornJavaParserTest.java
+++ typescript-generator-core/src/test/java/cz/habarta/typescript/generator/NashornJavaParserTest.java
@@ -1,7 +1,10 @@
 package cz.habarta.typescript.generator;
 
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.junit.Test;
 
@@ -28,19 +31,39 @@ public class NashornJavaParserTest {
             return privateBooleanProperty;
         }
         
-        public String someMethod(String arg1, int arg2, List<String> arg3) {
+        public String someMethod(String nameArg, int someNumArg, List<String> arg3) {
             return "";
         }        
+        
+        public Iterator<OtherDummy> getIteratorMethod(String x) {
+            return null;
+        }
+
+        public Iterator<OtherDummy> getIteratorGetter() {
+            return null;
+        }
+        
+        public void callbackMethod(Consumer<String> consumer) {
+            consumer.accept("Hello");
+        }    
+    }
+    
+    public static class OtherDummy {
+        public String getName() {
+            return "Hello";
+        }
     }
     
     @Test
-    public void test() {
+    public void testMyDummyBean() {
         final StringWriter stringWriter = new StringWriter();
         Settings settings = new Settings();        
         
-        settings.mapPackagesToNamespaces = true;
+        settings.namespace = "Java";
+        settings.outputKind = TypeScriptOutputKind.global;
+        settings.mapPackagesToNamespaces = true;     
         
-        DefaultTypeProcessor typeProcessor = new DefaultTypeProcessor();
+        TypeProcessor typeProcessor = getTypeProcessor(settings);
         NashornJavaParser parser = new NashornJavaParser(settings, typeProcessor);
         ModelCompiler modelCompiler = new ModelCompiler(settings, typeProcessor);
         Emitter emitter = new Emitter(settings);
@@ -53,6 +76,40 @@ public class NashornJavaParserTest {
         System.out.println("tsModel="+tsModel);
         System.out.println(stringWriter.toString());
     }    
+    
+    @Test
+    public void testArrayList() {
+        final StringWriter stringWriter = new StringWriter();
+        Settings settings = new Settings();        
+        
+        settings.namespace = "Java";
+        settings.outputKind = TypeScriptOutputKind.global;
+        settings.mapPackagesToNamespaces = true;
+        
+        TypeProcessor typeProcessor = getTypeProcessor(settings);
+        NashornJavaParser parser = new NashornJavaParser(settings, typeProcessor);
+        ModelCompiler modelCompiler = new ModelCompiler(settings, typeProcessor);
+        Emitter emitter = new Emitter(settings);
+        final Class<?> bean = ArrayList.class;
+        final Model model = parser.parseModel(bean);
+        final TsModel tsModel = modelCompiler.javaToTypeScript(model);
+        emitter.emit(tsModel, stringWriter, "dummy", true, false, 0);
+
+        System.out.println("model="+model);
+        System.out.println("tsModel="+tsModel);
+        System.out.println(stringWriter.toString());
+    }
+    
  
+    private TypeProcessor getTypeProcessor(Settings settings) {
+        final List<TypeProcessor> processors = new ArrayList<>();
+        processors.add(new ExcludingTypeProcessor(settings.getExcludeFilter()));
+        if (settings.customTypeProcessor != null) {
+            processors.add(settings.customTypeProcessor);
+        }
+        processors.add(new CustomMappingTypeProcessor(settings.customTypeMappings));
+        processors.add(new DefaultTypeProcessor());
+        return new TypeProcessor.Chain(processors);
+    }    
     
 }
